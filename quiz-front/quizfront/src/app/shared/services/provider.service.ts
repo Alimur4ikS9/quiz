cb04e3ba341e9b0723961e23ebf08eab9d2518f2
Alimur4ikS9 <alinur.sabit.9@gmail.com>
2019-04-30 10:05:33 +0600

    initial

---
+++ quiz-front/quizfront/src/app/shared/services/provider.service.ts
@@ -0,0 +1,13 @@
+import { Injectable, EventEmitter } from '@angular/core';
+import {MainService} from "./main.service";
+import {HttpClient} from "@angular/common/http";
+import {ITaskList, ITask, IAuthResponse, IUser} from "../models/models";
+
+@Injectable({
+  providedIn: 'root'
+})
+
+export class ProviderService extends MainService{
+
+  
+}
